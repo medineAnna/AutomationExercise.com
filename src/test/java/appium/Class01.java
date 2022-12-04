5b5f4a0aeb4e3b80eaed84b72df3171a607f4dab
medineAnna <medine.ah>
2022-12-04 08:22:17 +0100

    Appium

--- src/test/java/appium/Class01.java
+++ src/test/java/appium/Class01.java
@@ -1,6 +1,7 @@
 package appium;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -21,15 +22,17 @@ public class Class01 {
         DesiredCapabilities desiredCapabilities=new DesiredCapabilities();
 
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android"); //Welche Platform ISO oder Android
-        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel3"); //es ist IOS wichtig
-        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"13.0"); //welche Applicatio mochte ich testen, schreibe ich hier die Version der App
+        //desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel5"); //es ist IOS wichtig
+        //desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"13.0"); //welche Applicatio mochte ich testen, schreibe ich hier die Version der App
         //wenn App Version 10, 10,8 ise 8
-        desiredCapabilities.setCapability(MobileCapabilityType.APPLICATION_NAME,"UiAutomator2");
+        //desiredCapabilities.setCapability(MobileCapabilityType.APPLICATION_NAME,"UiAutomator2");
         //Wir müssen UIAutomator2 für Android schreiben, es nimmt es und leitet diese Anfrage im JSON-Format an den Appium-Server weiter
         desiredCapabilities.setCapability(MobileCapabilityType.APP,"/Users/medineannamammedova/Documents/AutomationExercise.com/src/app/app-release.apk");
         //ich habe gestutenin path geschrieben, die Codes in das Gerät einzuführen. Ich habe es in APP eingeführt
 
-        AndroidDriver<WebElement> driver = new AndroidDriver<WebElement>(new URL("http:127.0.0.1:4723/wd/hub"),desiredCapabilities);
+        //desiredCapabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.example.yeshasprabhakar.todo");
+
+        AndroidDriver<WebElement> driver = new AndroidDriver<WebElement>(new URL("http://localhost:4723/wd/hub"),desiredCapabilities);
         //webElemet yerine MobilWebElemet,AndroindWebelement man kann schreiben
         //appium url geschrieben
 
